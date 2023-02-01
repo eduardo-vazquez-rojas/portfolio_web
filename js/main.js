c0ccfe935c0443eaa7e0991ef38fecbc55a5b604
Eduardo Vazquez <arq.eduardovazquez@gmail.com>
2023-02-01 23:22:08 +0100

    primeros cambios

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["eduardo vazquez.", "Architect.", "3d Artist.", "Developer."],
       typeSpeed: 100,
       loop: true,
     });
